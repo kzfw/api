97e9c94a370797d1108692544f041c5465bdb698
adubcatc <austinrwilkins@gmail.com>
2023-02-14 00:03:34 -0600

    added ACT

--- config/zab.js
+++ config/zab.js
@@ -1,6 +1,6 @@
 export default {
-	atcPos: ["FTW", "DFW", "REG", "DAL", "OKC", "LBB", "ABI", "ADM", "ADS", "AFW", "BAD", "CNW", "CSM", "DTN", "DYS", "FSI", "FWS", "GGG", "GKY", "GPM", "GRK", "GVT", "GYI", "HLR", "HQZ", "HOB", "LAW", "LTS", "MAF", "MLU", "NFW", "OUN", "PWA", "RBD", "SHV", "SJT", "SPS", "TIK", "TKI", "TXK", "TYR"],
-	airports: ["KDFW","KDAL","KFTW","KOKC","KLBB","KABI","KADM","KADS","KAFW","KBAD","KCNW","KCSM","KDTN","KDYS","KFSI","KFWS","KGGG","KGKY","KGPM","KGRK","KGVT","KGYI","KHLR","KHQZ","KHOB","KLAW","KLTS","KMAF","KMLU","KNFW","KOUN","KPWA","KRBD","KSHV","KSJT","KSPS","KTIK","KTKI","KTXK","KTYR"],
+	atcPos: ["FTW", "DFW", "REG", "ACT", "DAL", "OKC", "LBB", "ABI", "ADM", "ADS", "AFW", "BAD", "CNW", "CSM", "DTN", "DYS", "FSI", "FWS", "GGG", "GKY", "GPM", "GRK", "GVT", "GYI", "HLR", "HQZ", "HOB", "LAW", "LTS", "MAF", "MLU", "NFW", "OUN", "PWA", "RBD", "SHV", "SJT", "SPS", "TIK", "TKI", "TXK", "TYR"],
+	airports: ["KDFW","KDAL","KFTW","KACT","KOKC","KLBB","KABI","KADM","KADS","KAFW","KBAD","KCNW","KCSM","KDTN","KDYS","KFSI","KFWS","KGGG","KGKY","KGPM","KGRK","KGVT","KGYI","KHLR","KHQZ","KHOB","KLAW","KLTS","KMAF","KMLU","KNFW","KOUN","KPWA","KRBD","KSHV","KSJT","KSPS","KTIK","KTKI","KTXK","KTYR"],
 	ratings: ["Unknown", "OBS", "S1", "S2", "S3", "C1", "C2", "C3", "I1", "I2", "I3", "SUP", "ADM"],
 	ratingsLong: ["Unknown", "Observer", "Student", "Student 2", "Senior Student", "Controller", "Controller 2", "Senior Controller", "Instructor", "Instructor 2", "Senior Instructor", "Supervisor", "Administrator"],
 };
